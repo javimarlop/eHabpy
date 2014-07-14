11694e255ab6a3817f24e6ca5887ce36c7ee83b2
Lucy Bastin <doctorluz@gmail.com>
2014-07-14 16:25:11 +0200

    new files for Tzar
    python.yaml contains parameter information, and model.py calls the code
    using those parameters

---
+++ model.py
@@ -0,0 +1,25 @@
+import csv
+import glob
+import os
+import sys
+import ehablib as ehab
+import basemodel
+
+class Model(basemodel.BaseModel):
+    def execute(self, runparams):
+        """Executes the model and writes the output to the CSV file specified in the project params.
+        """
+        # get parameters qualified by input and output file paths
+        params = runparams['parameters']
+
+        ecor = '60165'
+        
+        # Log the variable values
+#         self.logger.fine("random.seed: %s" % params["random.seed"])
+
+        ehab.ehabitat(ecor, params["shared_data_path"], output_dir)
+        
+#         # write some variables to output files
+#         with open(params["output.filename"], 'w') as f1:
+#             f1.write(str(params["test.variable.1"]) + "\n")
+# 
