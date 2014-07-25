e676765ca48691b0f3d5cb559bda00c336dd6144
Lucy Bastin <doctorluz@gmail.com>
2014-07-25 09:18:20 +0200

    Added a section that checks for the presence of the 'results' directory, and creates it if it does not exist. If this works we should move it into the core ehab code.

--- model.py
+++ model.py
@@ -16,6 +16,17 @@ class Model(basemodel.BaseModel):
         self.logger.fine("Output directory: %s" % params["output_dir"])
         od = params["output_dir"][:-1]
         self.logger.fine("New output directory: %s" % od)
+        
+        d = os.path.join(os.path.sep, od,'results')
+        
+        try:
+            os.makedirs(d)
+        except OSError:
+            if os.path.isdir(d):
+                self.logger.fine("Can't create directory: %s - a directory with this name already exists. It will be used for the results of the analysis." % d)  
+            else: 
+                self.logger.fine("Can't create directory: %s - halting the analysis as there is nowhere to write results." % d)  
+        
         ehab.ehabitat(params["ecoreg_id"], params["shared_dir"], od)
         
 #         # write some variables to output files
