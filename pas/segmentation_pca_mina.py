a6e09d880be345baec0ea29da7eb1624e364dc1c
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2014-10-17 14:50:37 +0200

    last

--- pas/segmentation_pca_mina.py
+++ pas/segmentation_pca_mina.py
@@ -11,15 +11,15 @@ GISBASE = os.environ['GISBASE'] = "/home/majavie/grass_last/new/grass7_trunk/dis
 GRASSDBASE = "/local1/majavie/hanksgrass7"
 MYLOC = "global_MW"
 mapset = 'ehabitat'
-col= 'wdpaid'
+col= 'WDPA_ID'#'wdpaid'
 
 sys.path.append(os.path.join(os.environ['GISBASE'], "etc", "python"))
 import grass.script as grass
 import grass.script.setup as gsetup
 
 gsetup.init(GISBASE, GRASSDBASE, MYLOC, mapset)
-
-source = 'wdpa_aug14_100km2_moll'
+# add rnd number to cat!!!
+source = 'wdpa_snapshot_new_mollweide'#'wdpa_aug14_100km2_moll'
 grass. message ("Extracting list of PAs")
 pa_list0 = grass. read_command ('v.db.select', map=source,column=col). splitlines ()
 pa_list2 = np.unique(pa_list0)
@@ -40,7 +40,7 @@ pa_list_done = np.genfromtxt(csvname1,dtype='string')
 n2 = len(pa_list)
 #print pa_list[1]
 for px in tqdm(range(0,n2)):
-
+ aleat = np.random.random_integers(100)
 #for pa in pa_list:
  pa = pa_list[px]
  if pa not in pa_list_done:
@@ -66,7 +66,7 @@ for px in tqdm(range(0,n2)):
   minarea = np.sqrt(int(a[1]))#/1000
   minaream = minarea*1000
   #print minarea
-  grass.run_command('i.pca', flags='n', input='pre,epr,slope,tree,herb,ndwi,ndvimax2,ndvimin,bio', output=pa44, overwrite=True) # dem
+  grass.run_command('i.pca', flags='n', input='pre,eprsqrt,slope,tree,herb,ndwi,ndvimax2,ndvimin,bio', output=pa44, overwrite=True) # dem
   pca1 = pa44+'.1'
   pca2 = pa44+'.2'
   pca3 = pa44+'.3'
@@ -84,8 +84,9 @@ for px in tqdm(range(0,n2)):
   b = grass.read_command('r.stats',input=pa3,flags='nc',separator='\n').splitlines()
   pa3b = pa3
   c = '-1'
+  clean = None
   for g in np.arange(1,len(b),2):
-   if b[g] < 6:
+   if b[g] < minarea:
     clean = 'T'
     c = c + ' || '+str(b[g-1])
   if clean == 'T':
@@ -97,12 +98,14 @@ for px in tqdm(range(0,n2)):
   #grass. message ("converting to vector")
   grass.run_command('r.to.vect', input=pa3b,out=pa4,type ='area',flags='v',overwrite=True)
   #grass. message ("adding labels to segments")
-  grass.run_command('v.db.addcolumn', map=pa4,col='wdpaid VARCHAR')
-  grass.run_command('v.db.update', map=pa4,col='wdpaid',value=pa)
+  grass.run_command('v.db.addcolumn', map=pa4,col='wdpaid_pa VARCHAR')
+  grass.run_command('v.db.update', map=pa4,col='wdpaid_pa',value=pa)
+  grass.run_command('v.db.addcolumn', map=pa4,col='aleat VARCHAR')
+  grass.run_command('v.db.update', map=pa4,col='aleat',value=aleat)
   #grass. message ("Checking shapefile")
   grass.run_command('v.clean', input=pa4,out=pa44,tool='rmarea',thres=minaream,overwrite=True)
-  grass.run_command('v.db.addcolumn', map=pa44,col='wdpa_id VARCHAR')
-  grass.run_command('v.db.update', map=pa44,col='wdpa_id',qcol='wdpaid || cat')
+  grass.run_command('v.db.addcolumn', map=pa44,col='wdpaid_segm VARCHAR')
+  grass.run_command('v.db.update', map=pa44,col='wdpaid_segm',qcol='wdpaid_pa || cat || aleat')
   #grass. message ("Exporting shapefile")
   if os.path.isfile('parks_segmented_pca.shp') == False:
    grass.run_command('v.out.ogr',input=pa44,ola='parks_segmented_pca',type='area',dsn='.') 
