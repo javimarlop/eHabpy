b18e1ab28c244346da4c300deb29807b1c59fb32
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2014-11-18 17:26:38 +0100

    last

---
+++ ehab_tzar_docker.sh
@@ -0,0 +1,33 @@
+docker pull javimarlop/ubuntugis-docker # clone docker image
+#docker run --rm -tiv /home/webuser/grassdb:/opt/grassdb javimarlop/ubuntugis-docker # with shared folder
+docker run --rm --privileged -ti javimarlop/ubuntugis-docker /bin/bash
+
+useradd -m majavie #--uid xxx
+#usermod -a -G fuse majavie
+usermod -s /bin/bash majavie
+
+su majavie
+cd /home/majavie
+mkdir /home/majavie/local0
+#mkdir /home/majavie/.ssh
+#echo '139.191.147.102' > /home/majavie/.ssh/hosts
+
+sshfs xxx@yyy:/local0/majavie/ /home/majavie/local0
+
+wget https://tzar-framework.atlassian.net/wiki/download/attachments/4980739/tzar-0.5.4.jar?version=1&modificationDate=1405994845498&api=v2
+
+mv tzar-0.5.4.jar\?version\=1 tzar.jar
+
+#easy_install -U distribute
+
+#pip install matplotlib
+
+#java -jar tzar.jar execlocalruns http://rdv-framework.googlecode.com/svn/trunk/projects/lucy_morph
+
+java -jar tzar.jar execlocalruns https://github.com/javimarlop/eHabpy/trunk
+
+
+
+###
+
+
