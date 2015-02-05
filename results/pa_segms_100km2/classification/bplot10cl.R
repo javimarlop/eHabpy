b04f2c0e6b6fc54edbe376cb6b963cdfd14e347a
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2015-02-05 17:21:50 +0100

    last

--- results/pa_segms_100km2/classification/bplot10cl.R
+++ results/pa_segms_100km2/classification/bplot10cl.R
@@ -4,3 +4,17 @@ png(paste(names(hri)[i],'_10cl_boxplots.png',sep=''))
 boxplot(hri[,i] ~ hclust_dist_euclid_segms_10cl,main=paste(names(hri)[i]))
 dev.off()
 }}
+
+bplot10cl2<-function(){
+for(i in 14:40){
+png(paste(names(hri)[i],'_10cl_boxplots_mh.png',sep=''))
+boxplot(hri[,i] ~ hclust_dist_euclid_segms_10cl2,main=paste(names(hri)[i]))
+dev.off()
+}}
+
+bplot10cl3<-function(){
+for(i in c(3,14:40)){
+png(paste(names(hri)[i],'_10cl_boxplots_mh_all.png',sep=''))
+boxplot(hri[,i] ~ hclust_dist_euclid_segms_10cl3,main=paste(names(hri)[i]))
+dev.off()
+}}
